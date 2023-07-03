d864dc6d344d635ca28d6adea02a1f518ace80a1
jose-uchoa <j-uchoa@hotmail.com>
2023-07-03 13:19:02 -0300

    feat: new config for pytest

--- order/tests/test_order_viewsets/test_order_viewset.py
+++ order/tests/test_order_viewsets/test_order_viewset.py
@@ -47,3 +47,5 @@ class TestOrderViewSet(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
         created_order = Order.objects.get(user=user)
+
+        self.assertEqual(created_order.user, user)
