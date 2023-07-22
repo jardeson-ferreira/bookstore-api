c5ddb242c4f8a3b0fe5b9ca2f1fa7993258c546a
José Uchôa <jooseuchoa@gmail.com>
2023-07-22 00:55:29 -0300

    feat: deploy

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
+    # "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = "bookstore.urls"
